4f5fcdb062c178f26ea68f94458d5d232e678745
Sandro Maiate <sg.maiat@gmail.com>
2022-02-02 21:32:46 -0300

    added projects page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,38 @@
 const contact = document.getElementById("contact-box");
 const information = document.getElementById("information-box");
+const projects = document.getElementById("projects");
+const profile = document.getElementById("profile-card");
 
 function displayInformations() {
+  profile.classList.remove("non-display");
   information.classList.remove("non-display");
   contact.classList.add("non-display");
   information.classList.add("display");
+  projects.style.display = "none";
 }
 function displayContact() {
+  profile.classList.remove("non-display");
+  projects.style.display = "none";
   contact.classList.remove("non-display");
   information.classList.add("non-display");
 }
-// function displayKnowledge();
-// function displayProjects();
+
+function displayProjects() {
+  profile.classList.add("non-display");
+  information.classList.add("non-display");
+  contact.classList.add("non-display");
+  projects.style.display = "flex";
+}
+let count = 0;
+function changeWindow() {
+  if (count == 0) {
+    displayContact();
+    count++;
+  } else if (count == 1) {
+    displayProjects();
+    count++;
+  } else {
+    displayInformations();
+    count = 0;
+  }
+}
